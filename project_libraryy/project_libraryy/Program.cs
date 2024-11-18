cdee68168288c7dd15a0711f44d1722d383dcfbf
Amani <amanialshmali7@gmail.com>
2024-11-18 14:28:50 +0400

    mmaping

---
+++ project_libraryy/project_libraryy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_libraryy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
