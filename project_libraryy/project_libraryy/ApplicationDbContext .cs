3ad333c0a95e0ca207795d56078845b01fde41a0
Amani <amanialshmali7@gmail.com>
2024-11-18 14:56:13 +0400

    adding UserRepository

---
+++ project_libraryy/project_libraryy/ApplicationDbContext .cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using project_libraryy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_libraryy
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(local); Initial Catalog=project_LIBRARY; Integrated Security=true; TrustServerCertificate=True");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Borrowing> Borrowings { get; set; }
+    }
+}
