b9da89fc0f20ae8400a896a06ec9851f5c858b87
Amani <amanialshmali7@gmail.com>
2024-11-18 14:37:19 +0400

    adding catagery modle

---
+++ project_libraryy/project_libraryy/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_libraryy.Models
+{
+    public class Category
+    {
+        [Key]
+        public int C_ID { get; set; }
+
+        [Required]
+        public string C_Name { get; set; }
+
+        [Required]
+        public int C_NumberOfBooks { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
