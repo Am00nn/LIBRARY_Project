e19372ec909fe72d073ed4d341544adc90186088
Amani <amanialshmali7@gmail.com>
2024-11-18 14:36:16 +0400

    add admin modle

---
+++ project_libraryy/project_libraryy/Models/Admin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_libraryy.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int A_ID { get; set; }
+
+        [Required]
+        public string A_Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string A_Email { get; set; }
+
+
+        [Required]
+        [MinLength(8)]
+
+        public string A_Password { get; set; }
+    }
+}
