6d3b915bbf5f6cf54a0a7a2df7eee71c1ab5c684
Amani <amanialshmali7@gmail.com>
2024-11-18 14:37:00 +0400

    adding borrowing modle

---
+++ project_libraryy/project_libraryy/Models/Borrowing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_libraryy.Models
+{
+    public class Borrowing
+    {
+        [Key]
+        public int Borrowing_ID { get; set; }
+
+        [Required]
+        public DateTime B_BorrowingDate { get; set; }
+
+        [Required]
+        public DateTime B_PredictedReturnDate { get; set; }
+
+        public DateTime? B_ActualReturnDate { get; set; }
+
+        public int? Rating { get; set; }
+
+        [Required]
+        public bool IsReturned { get; set; }
+
+        [ForeignKey("User")]
+        public int U_ID { get; set; }
+        public virtual User User { get; set; }
+
+        [ForeignKey("Book")]
+        public int Book_ID { get; set; }
+        public virtual Book Book { get; set; }
+    }
+}
